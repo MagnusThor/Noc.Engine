3ff0bfa5af3b74fa40f886e95f685b2b043ce314
Magnus Thor <magnus@xsockets.net>
2015-12-30 19:21:34 +0100

    .
    .

--- test.js
+++ test.js
@@ -1,330 +1,210 @@
-﻿
-float hash( float n ) { return fract(sin(n)*13.5453123); }
+﻿/* Copyright (C) 2013 Justin Windle, http://soulwire.co.uk */
+var Sketch = function () {
+    "use strict";
 
-float maxcomp( in vec3 v ) { return max( max( v.x, v.y ), v.z ); }
+    function e(e) {
+        return "[object Array]" == Object.prototype.toString.call(e)
+    }
 
-float dbox( vec3 p, vec3 b, float r )
-{
-    return length(max(abs(p)-b,0.0))-r;
-}
+    function t(e) {
+        return "function" == typeof e
+    }
 
-vec4 texcube( sampler2D sam, in vec3 p, in vec3 n )
-{
-	vec4 x = texture2D( sam, p.yz );
-vec4 y = texture2D( sam, p.zx );
-vec4 z = texture2D( sam, p.yx );
-vec3 a = abs(n);
-return (x*a.x + y*a.y + z*a.z) / (a.x + a.y + a.z);
-}
+    function n(e) {
+        return "number" == typeof e
+    }
 
-//---------------------------------
+    function o(e) {
+        return "string" == typeof e
+    }
 
-float freqs[4];
+    function r(e) {
+        return E[e] || String.fromCharCode(e)
+    }
 
-vec3 mapH( in vec2 pos )
-{
-	vec2 fpos = fract( pos ); 
-vec2 ipos = floor( pos );
-	
-float f = 0.0;	
-float id = hash( ipos.x + ipos.y*57.0 );
-f += freqs[0] * clamp(1.0 - abs(id-0.20)/0.30, 0.0, 1.0 );
-f += freqs[1] * clamp(1.0 - abs(id-0.40)/0.30, 0.0, 1.0 );
-f += freqs[2] * clamp(1.0 - abs(id-0.60)/0.30, 0.0, 1.0 );
-f += freqs[3] * clamp(1.0 - abs(id-0.80)/0.30, 0.0, 1.0 );
+    function i(e, t, n) {
+        for (var o in t) (n || !e.hasOwnProperty(o)) && (e[o] = t[o]);
+        return e
+    }
 
-f = pow( clamp( f, 0.0, 1.0 ), 2.0 );
-float h = 2.5*f;
+    function u(e, t) {
+        return function () {
+            e.apply(t, arguments)
+        }
+    }
 
-return vec3( h, id, f );
-}
+    function a(e) {
+        var n = {};
+        for (var o in e) n[o] = t(e[o]) ? u(e[o], e) : e[o];
+        return n
+    }
 
-vec3 map( in vec3 pos )
-{
-	vec2  p = fract( pos.xz ); 
-vec3  m = mapH( pos.xz );
-float d = dbox( vec3(p.x-0.5,pos.y-0.5*m.x,p.y-0.5), vec3(0.3,m.x*0.5,0.3), 0.1 );
-return vec3( d, m.yz );
-}
+    function c(e) {
+        function n(n) {
+            t(n) && n.apply(e, [].splice.call(arguments, 1))
+        }
 
-const float surface = 0.001;
+        function u(e) {
+            for (_ = 0; _ < J.length; _++) G = J[_], o(G) ? O[(e ? "add" : "remove") + "EventListener"].call(O, G, k, !1) : t(G) ? k = G : O = G
+        }
 
-#if 0
-    vec3 trace( in vec3 ro, in vec3 rd, in float startf, in float maxd )
-{ 
-        float s = surface*2.0;
-float t = startf;
+        function c() {
+            L(T), T = I(c), U || (n(e.setup), U = t(e.setup), n(e.resize)), e.running && !j && (e.dt = (B = +new Date) - e.now, e.millis += e.dt, e.now = B, n(e.update), e.autoclear && K && e.clear(), n(e.draw)), j = ++j % e.interval
+        }
 
-float sid = -1.0;
-float alt = 0.0;
-for( int i=0; i<128; i++ )
-{
-    if( s<surface || t>maxd ) break;
-    t += 0.15*s;
-    vec3 res = map( ro + rd*t );
-    s   = res.x;
-    sid = res.y;
-    alt = res.z;
-}
-if( t>maxd ) sid = -1.0;
-return vec3( t, sid, alt );
-}
+        function l() {
+            O = Y ? e.style : e.canvas, D = Y ? "px" : "", e.fullscreen && (e.height = w.innerHeight, e.width = w.innerWidth), O.height = e.height + D, O.width = e.width + D, e.retina && K && X && (O.height = e.height * X, O.width = e.width * X, O.style.height = e.height + "px", O.style.width = e.width + "px", e.scale(X, X)), U && n(e.resize)
+        }
 
-#else
+        function s(e, t) {
+            return N = t.getBoundingClientRect(), e.x = e.pageX - N.left - w.scrollX, e.y = e.pageY - N.top - w.scrollY, e
+        }
 
-vec3 trace( vec3 ro, in vec3 rd, in float tmin, in float tmax )
-{
-ro += tmin*rd;
-    
-vec2 pos = floor(ro.xz);
-vec3 rdi = 1.0/rd;
-vec3 rda = abs(rdi);
-vec2 rds = sign(rd.xz);
-vec2 dis = (pos-ro.xz+ 0.5 + rds*0.5) * rdi.xz;
-	
-vec3 res = vec3( -1.0 );
+        function f(t, n) {
+            return s(t, e.element), n = n || {}, n.ox = n.x || t.x, n.oy = n.y || t.y, n.x = t.x, n.y = t.y, n.dx = n.x - n.ox, n.dy = n.y - n.oy, n
+        }
 
-    // traverse regular grid (in 2D)
-vec2 mm = vec2(0.0);
-for( int i=0; i<28; i++ ) 
-{
-    vec3 cub = mapH( pos );
+        function g(e) {
+            if (e.preventDefault(), W = a(e), W.originalEvent = e, W.touches)
+                for (M.length = W.touches.length, _ = 0; _ < W.touches.length; _++) M[_] = f(W.touches[_], M[_]);
+            else M.length = 0, M[0] = f(W, V);
+            return i(V, M[0], !0), W
+        }
 
-    #if 1
-        vec2 pr = pos+0.5-ro.xz;
-        vec2 mini = (pr-0.5*rds)*rdi.xz;
-        float s = max( mini.x, mini.y );
-        if( (tmin+s)>tmax ) break;
-    #endif
-        
-        
-    // intersect box
-    vec3  ce = vec3( pos.x+0.5, 0.5*cub.x, pos.y+0.5 );
-    vec3  rb = vec3(0.3,cub.x*0.5,0.3);
-    vec3  ra = rb + 0.12;
-    vec3  rc = ro - ce;
-    float tN = maxcomp( -rdi*rc - rda*ra );
-    float tF = maxcomp( -rdi*rc + rda*ra );
-    if( tN < tF )//&& tF > 0.0 )
-{
-    // raymarch
-        float s = tN;
-        float h = 1.0;
-        for( int j=0; j<24; j++ )
-{
-            h = dbox( rc+s*rd, rb, 0.1 ); 
-            s += h;
-            if( s>tF ) break;
-}
+        function h(t) {
+            for (t = g(t), q = (Q = J.indexOf(z = t.type)) - 1, e.dragging = /down|start/.test(z) ? !0 : /up|end/.test(z) ? !1 : e.dragging; q;) o(J[q]) ? n(e[J[q--]], t) : o(J[Q]) ? n(e[J[Q++]], t) : q = 0
+        }
 
-        if( h < (surface*s*2.0) )
-{
-            res = vec3( s, cub.yz );
-            break; 
-}
-            
-}
+        function p(t) {
+            F = t.keyCode, H = "keyup" == t.type, Z[F] = Z[r(F)] = !H, n(e[t.type], t)
+        }
 
-        
-    // step to next cell		
-    mm = step( dis.xy, dis.yx ); 
-    dis += mm*rda.xz;
-    pos += mm*rds;
-}
+        function v(t) {
+            e.autopause && ("blur" == t.type ? b : C)(), n(e[t.type], t)
+        }
 
-res.x += tmin;
-    
-return res;
-}
-#endif
+        function C() {
+            e.now = +new Date, e.running = !0
+        }
 
+        function b() {
+            e.running = !1
+        }
 
-float softshadow( in vec3 ro, in vec3 rd, in float mint, in float maxt, in float k )
-{
-float res = 1.0;
-float t = mint;
-for( int i=0; i<50; i++ )
-{
-    float h = map( ro + rd*t ).x;
-    res = min( res, k*h/t );
-    t += clamp( h, 0.05, 0.2 );
-    if( res<0.001 || t>maxt ) break;
-}
-return clamp( res, 0.0, 1.0 );
-}
+        function P() {
+            (e.running ? b : C)()
+        }
 
-vec3 calcNormal( in vec3 pos, in float t )
-{
-vec2 e = vec2(1.0,-1.0)*surface*t;
-return normalize( e.xyy*map( pos + e.xyy ).x + 
-                  e.yyx*map( pos + e.yyx ).x + 
-                  e.yxy*map( pos + e.yxy ).x + 
-                  e.xxx*map( pos + e.xxx ).x );
-}
+        function A() {
+            K && e.clearRect(0, 0, e.width, e.height)
+        }
 
-const vec3 light1 = vec3(  0.70, 0.52, -0.45 );
-const vec3 light2 = vec3( -0.71, 0.000,  0.71 );
-const vec3 lpos = vec3(0.0) + 6.0*light1;
-
-vec2 boundingVlume( vec2 tminmax, in vec3 ro, in vec3 rd )
-{
-float bp = 2.7;
-float tp = (bp-ro.y)/rd.y;
-if( tp>0.0 ) 
-{
-    if( ro.y>bp ) tminmax.x = max( tminmax.x, tp );
-else          tminmax.y = min( tminmax.y, tp );
-}
-bp = 0.0;
-tp = (bp-ro.y)/rd.y;
-if( tp>0.0 ) 
-{
-    if( ro.y>bp ) tminmax.y = min( tminmax.y, tp );
-}
-return tminmax;
-}
-
-
-vec3 doLighting( in vec3 col, in float ks,
-             in vec3 pos, in vec3 nor, in vec3 rd )
-{
-vec3  ldif = pos - lpos;
-float llen = length( ldif );
-ldif /= llen;
-float con = dot(-light1,ldif);
-float occ = mix( clamp( pos.y/4.0, 0.0, 1.0 ), 1.0, max(0.0,nor.y) );
-vec2 sminmax = vec2(0.01, 5.0);
-    //sminmax = boundingVlume( sminmax, pos, -ldif );
-float sha = softshadow( pos, -ldif, sminmax.x, sminmax.y, 32.0 );;
-		
-float bb = smoothstep( 0.5, 0.8, con );
-float lkey = clamp( dot(nor,-ldif), 0.0, 1.0 );
-vec3  lkat = vec3(1.0);
-      lkat *= vec3(bb*bb*0.6+0.4*bb,bb*0.5+0.5*bb*bb,bb).zyx;
-      lkat /= 1.0+0.25*llen*llen;		
-      lkat *= 25.0;
-      lkat *= sha;
-float lbac = clamp( 0.1 + 0.9*dot( light2, nor ), 0.0, 1.0 );
-      lbac *= smoothstep( 0.0, 0.8, con );
-      lbac /= 1.0+0.2*llen*llen;		
-      lbac *= 4.0;
-float lamb = 1.0 - 0.5*nor.y;
-      lamb *= 1.0-smoothstep( 10.0, 25.0, length(pos.xz) );
-      lamb *= 0.25 + 0.75*smoothstep( 0.0, 0.8, con );
-      lamb *= 0.25;
-		
-vec3 lin  = 1.0*vec3(0.20,0.05,0.02)*lamb*occ;
-     lin += 1.0*vec3(1.60,0.70,0.30)*lkey*lkat*(0.5+0.5*occ);
-     lin += 1.0*vec3(0.70,0.20,0.08)*lbac*occ;
-     lin *= vec3(1.3,1.1,1.0);
-
-col = col*lin;
-
-vec3 spe = vec3(1.0)*occ*lkat*pow( clamp(dot( reflect(rd,nor), -ldif  ),0.0,1.0), 4.0 );
-col += (0.5+0.5*ks)*0.5*spe*vec3(1.0,0.9,0.7);
-
-return col;
-}
-
-mat3 setLookAt( in vec3 ro, in vec3 ta, float cr )
-{
-vec3  cw = normalize(ta-ro);
-vec3  cp = vec3(sin(cr), cos(cr),0.0);
-vec3  cu = normalize( cross(cw,cp) );
-vec3  cv = normalize( cross(cu,cw) );
-return mat3( cu, cv, cw );
-}
-
-vec3 render( in vec3 ro, in vec3 rd )
-{
-vec3 col = vec3( 0.0 );
-
-vec2 tminmax = vec2(0.0, 40.0 );
-
-tminmax = boundingVlume( tminmax, ro, rd );
-
-    // raytrace
-vec3 res = trace( ro, rd, tminmax.x, tminmax.y );
-if( res.y > -0.5 )
-{
-    float t = res.x;
-    vec3 pos = ro + t*rd;
-    vec3 nor = calcNormal( pos, t );
-
-    // material	
-    col = 0.5 + 0.5*cos( 6.2831*res.y + vec3(0.0, 0.4, 0.8) );
-    vec3 ff = texcube( iChannel1, 0.1*vec3(pos.x,4.0*res.z-pos.y,pos.z), nor ).xyz;
-    col *= ff.x;
-
-    // lighting
-    col = doLighting( col, ff.x, pos, nor, rd );
-    col *= 1.0 - smoothstep( 20.0, 40.0, t );
-}
-return col;
-}
-
-
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-freqs[0] = texture2D( iChannel0, vec2( 0.01, 0.25 ) ).x;
-freqs[1] = texture2D( iChannel0, vec2( 0.07, 0.25 ) ).x;
-freqs[2] = texture2D( iChannel0, vec2( 0.15, 0.25 ) ).x;
-freqs[3] = texture2D( iChannel0, vec2( 0.30, 0.25 ) ).x;
-
-    //-----------
-float time = 5.0 + 0.2*iGlobalTime + 20.0*iMouse.x/iResolution.x;
-
-vec3 tot = vec3(0.0);
-#ifdef ANTIALIAS
-for( int i=0; i<4; i++ )
-{
-    vec2 off = vec2( mod(float(i),2.0), mod(float(i/2),2.0) )/2.0;
-#else
-    vec2 off = vec2(0.0);
-#endif        
-    vec2 xy = (-iResolution.xy+2.0*(fragCoord.xy+off)) / iResolution.y;
-
-    // camera	
-    vec3 ro = vec3( 8.5*cos(0.2+.33*time), 5.0+2.0*cos(0.1*time), 8.5*sin(0.1+0.37*time) );
-    vec3 ta = vec3( -2.5+3.0*cos(1.2+.41*time), 0.0, 2.0+3.0*sin(2.0+0.38*time) );
-    float roll = 0.2*sin(0.1*time);
-
-    // camera tx
-    mat3 ca = setLookAt( ro, ta, roll );
-    vec3 rd = normalize( ca * vec3(xy.xy,1.75) );
-        
-    vec3 col = render( ro, rd );
-        
-    tot += pow( col, vec3(0.4545) );
-#ifdef ANTIALIAS
-}
-tot /= 4.0;
-#endif    
-    
-    // vigneting
-vec2 q = fragCoord.xy/iResolution.xy;
-tot *= 0.2 + 0.8*pow( 16.0*q.x*q.y*(1.0-q.x)*(1.0-q.y), 0.1 );
-
-fragColor=vec4( tot, 1.0 );
-
-
-
-
-}
-
-
-
-
-void mainVR( out vec4 fragColor, in vec2 fragCoord, in vec3 fragRayOri, in vec3 fragRayDir )
-{
-freqs[0] = texture2D( iChannel0, vec2( 0.01, 0.25 ) ).x;
-freqs[1] = texture2D( iChannel0, vec2( 0.07, 0.25 ) ).x;
-freqs[2] = texture2D( iChannel0, vec2( 0.15, 0.25 ) ).x;
-freqs[3] = texture2D( iChannel0, vec2( 0.30, 0.25 ) ).x;
-
-vec3 col = render( fragRayOri + vec3(0.0,4.0,0.0), fragRayDir );
-
-col += pow( col, vec3(0.4545) );
-
-fragColor = vec4( col, 1.0 );
-}
+        function S() {
+            R = e.element.parentNode, _ = x.indexOf(e), R && R.removeChild(e.element), ~_ && x.splice(_, 1), u(!1), b()
+        }
+        var T, k, O, R, N, _, D, B, G, W, z, F, H, q, Q, j = 0,
+			M = [],
+			U = !1,
+			X = w.devicePixelRatio,
+			Y = e.type == m,
+			K = e.type == d,
+			V = {
+			    x: 0,
+			    y: 0,
+			    ox: 0,
+			    oy: 0,
+			    dx: 0,
+			    dy: 0
+			},
+			J = [e.element, h, "mousedown", "touchstart", h, "mousemove", "touchmove", h, "mouseup", "touchend", h, "click", y, p, "keydown", "keyup", w, v, "focus", "blur", l, "resize"],
+			Z = {};
+        for (F in E) Z[E[F]] = !1;
+        return i(e, {
+            touches: M,
+            mouse: V,
+            keys: Z,
+            dragging: !1,
+            running: !1,
+            millis: 0,
+            now: 0 / 0,
+            dt: 0 / 0,
+            destroy: S,
+            toggle: P,
+            clear: A,
+            start: C,
+            stop: b
+        }), x.push(e), e.autostart && C(), u(!0), l(), c(), e
+    }
+    for (var l, s, f = "E LN10 LN2 LOG2E LOG10E PI SQRT1_2 SQRT2 abs acos asin atan ceil cos exp floor log round sin sqrt tan atan2 pow max min".split(" "), g = "__hasSketch", h = Math, d = "canvas", p = "webgl", m = "dom", y = document, w = window, x = [], v = {
+        fullscreen: !0,
+        autostart: !0,
+        autoclear: !0,
+        autopause: !0,
+        container: y.body,
+        interval: 1,
+        globals: !0,
+        retina: !1,
+        type: d
+    }, E = {
+			8: "BACKSPACE",
+			9: "TAB",
+			13: "ENTER",
+			16: "SHIFT",
+			27: "ESCAPE",
+			32: "SPACE",
+			37: "LEFT",
+			38: "UP",
+			39: "RIGHT",
+			40: "DOWN"
+    }, C = {
+        CANVAS: d,
+        WEB_GL: p,
+        WEBGL: p,
+        DOM: m,
+        instances: x,
+        install: function (t) {
+				if (!t[g]) {
+					for (var o = 0; o < f.length; o++) t[f[o]] = h[f[o]];
+					i(t, {
+        TWO_PI: 2 * h.PI,
+        HALF_PI: h.PI / 2,
+        QUATER_PI: h.PI / 4,
+        random: function (t, o) {
+							return e(t) ? t[~~(h.random() * t.length)] : (n(o) || (o = t || 1, t = 0), t + h.random() * (o - t))
+    },
+        lerp: function (e, t, n) {
+							return e + n * (t - e)
+    },
+        map: function (e, t, n, o, r) {
+							return (e - t) / (n - t) * (r - o) + o
+    }
+    }), t[g] = !0
+    }
+    },
+        create: function (e) {
+				return e = i(e || {}, v), e.globals && C.install(self), l = e.element = e.element || y.createElement(e.type === m ? "div" : "canvas"), s = e.context = e.context || function () {
+					switch (e.type) {
+						case d:
+							return l.getContext("2d", e);
+						case p:
+							return l.getContext("webgl", e) || l.getContext("experimental-webgl", e);
+						case m:
+							return l.canvas = l
+    }
+    }(), e.container.appendChild(l), C.augment(s, e)
+    },
+        augment: function (e, t) {
+				return t = i(t || {}, v), t.element = e.canvas || e, t.element.className += " sketch", i(e, t, !0), c(e)
+    }
+    }, b = ["ms", "moz", "webkit", "o"], P = self, A = 0, S = "AnimationFrame", T = "request" + S, k = "cancel" + S, I = P[T], L = P[k], O = 0; O < b.length && !I; O++) I = P[b[O] + "Request" + S], L = P[b[O] + "Cancel" + T];
+    return P[T] = I = I || function (e) {
+        var t = +new Date,
+			n = h.max(0, 16 - (t - A)),
+			o = setTimeout(function () {
+			    e(t + n)
+			}, n);
+        return A = t + n, o
+    }, P[k] = L = L || function (e) {
+        clearTimeout(e)
+    }, C
+}();
